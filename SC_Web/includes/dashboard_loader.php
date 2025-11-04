d9225f34fa5ffeb7cd2206abe6167083be0db588
Amy Gooch <amy.a.gooch@gmail.com>
2025-11-04 09:16:30 -0600

    fixing

--- SC_Web/includes/dashboard_loader.php
+++ SC_Web/includes/dashboard_loader.php
@@ -270,18 +270,21 @@ function displayWelcomeScreen($user) {
 
 /**
  * Format file size helper
+ * Note: This function may already be defined in dataset_list.php
  */
-function formatFileSize($bytes) {
-    if ($bytes == 0) return '0 B';
-    
-    $units = ['B', 'KB', 'MB', 'GB', 'TB'];
-    $bytes = max($bytes, 0);
-    $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
-    $pow = min($pow, count($units) - 1);
-    
-    $bytes /= pow(1024, $pow);
-    
-    return round($bytes, 2) . ' ' . $units[$pow];
+if (!function_exists('formatFileSize')) {
+    function formatFileSize($bytes) {
+        if ($bytes == 0) return '0 B';
+        
+        $units = ['B', 'KB', 'MB', 'GB', 'TB'];
+        $bytes = max($bytes, 0);
+        $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+        $pow = min($pow, count($units) - 1);
+        
+        $bytes /= pow(1024, $pow);
+        
+        return round($bytes, 2) . ' ' . $units[$pow];
+    }
 }
 ?>
 
