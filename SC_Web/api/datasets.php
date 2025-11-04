08ca5381a48b7b74ee97d453c11a3460288d2946
Amy Gooch <amy.a.gooch@gmail.com>
2025-11-04 10:49:03 -0600

    fxing session issue

--- SC_Web/api/datasets.php
+++ SC_Web/api/datasets.php
@@ -17,6 +17,11 @@ ob_start();
 ini_set('display_errors', 0);
 ini_set('display_startup_errors', 0);
 
+// Start session BEFORE including config.php to ensure session is available
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
 require_once(__DIR__ . '/../config.php');
 require_once(__DIR__ . '/../includes/auth.php');
 require_once(__DIR__ . '/../includes/dataset_manager.php');
