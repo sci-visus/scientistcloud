748326c50e2ea893762006d5625368158d894c17
Amy Gooch <amy.a.gooch@gmail.com>
2025-11-04 10:32:51 -0600

    fixing

--- SC_Web/config.php
+++ SC_Web/config.php
@@ -183,11 +183,13 @@ if (isset($_SERVER['HTTP_ORIGIN'])) {
     header('Access-Control-Max-Age: 86400');
 }
 
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-    exit(0);
-}
-?>
+// Don't handle OPTIONS here - let API endpoints handle it
+// This prevents conflicts with API-specific OPTIONS handling
+// if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+//     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+//         header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+//     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+//         header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+//     exit(0);
+// }
+// Note: No closing ?> tag to prevent any whitespace after the file from being output
