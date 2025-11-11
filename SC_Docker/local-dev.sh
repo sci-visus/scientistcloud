7240a9dfc0ad92e672a7fd01753e079fb6a1e4ff
Amy Gooch <amy.a.gooch@gmail.com>
2025-11-11 12:51:41 -0600

    fixing

--- SC_Docker/local-dev.sh
+++ SC_Docker/local-dev.sh
@@ -78,7 +78,7 @@ start_portal() {
     fi
     
     # Build and start portal
-    docker-compose -f docker-compose.yml build
+    docker-compose -f docker-compose.yml build --no-cache
     docker-compose -f docker-compose.yml up -d scientistcloud-portal
     
     # Wait for portal to be ready
