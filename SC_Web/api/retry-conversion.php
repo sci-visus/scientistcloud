d4641e169f24d38e7f6bc812b7b02d833bfba9c5
Amy Gooch <amy.a.gooch@gmail.com>
2025-11-20 13:12:23 -0600

    fiixng

--- SC_Web/api/retry-conversion.php
+++ SC_Web/api/retry-conversion.php
@@ -23,7 +23,7 @@ if (session_status() == PHP_SESSION_NONE) {
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
 // Handle preflight requests
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
